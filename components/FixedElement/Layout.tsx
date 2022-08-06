72f6918a313f75b1f676cbff70f84eaba931e2a2
seri0701 <76086657+seri0701@users.noreply.github.com>
2022-08-06 11:10:34 +0900

    🎨 練習メニュー修正

--- components/FixedElement/Layout.tsx
+++ components/FixedElement/Layout.tsx
@@ -12,11 +12,9 @@ export const Layout: FC<Props> = ({ children, title = '陸上競技' }) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <header></header>
       <main className="mx-auto min-h-screen max-w-2xl flex-col items-center px-2">
         {children}
       </main>
-      <footer></footer>
     </div>
   )
 }
