33c60cd17d38a8aa82a58108fd39f7db63de70bc
seri0701 <76086657+seri0701@users.noreply.github.com>
2022-09-11 08:03:40 +0900

    🎨 googleSearchConsoleを設定

--- components/FixedElement/Layout.tsx
+++ components/FixedElement/Layout.tsx
@@ -11,6 +11,10 @@ export const Layout: FC<Props> = ({ children, title = "陸上競技" }) => {
     <div className="flex min-h-screen">
       <Head>
         <title>{title}</title>
+        <meta
+          name="google-site-verification"
+          content="Qsv-aaCpuXDB2Xem4vIgTXcOco914A7epqBo6qJP9IQ"
+        />
       </Head>
       <main className="mx-auto max-w-2xl flex-col items-center px-2">
         {children}
