464dfa2026ed8d721af6dd71f9f5185807109704
seri0701 <76086657+seri0701@users.noreply.github.com>
2022-09-17 06:07:36 +0900

    🔨 幅跳び記事修正

--- pages/practice/longjamp.tsx
+++ pages/practice/longjamp.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next"
 
-import { Accordion } from "@mantine/core"
+import { Accordion, Anchor, Text } from "@mantine/core"
 
 import HeaderCom from "components/FixedElement/SubHeader/HeaderCom"
 import FooterCom from "components/FixedElement/FooterCom"
@@ -8,35 +8,85 @@ import { Layout } from "components/FixedElement/Layout"
 
 const Date = [
   {
+    Title: "助走",
+    Content:
+      "踏切を気にしたり疲れて楽に助走をしたり、毎回違う気持ちで走り出すと、スタートの一歩目から歩幅が毎回違ってしまうので気を付ける。",
+  },
+  {
+    Title: "助走２",
+    Content: "踏切手前で一番速い速度になるように走ってくる。",
+  },
+  {
     Title: "助走距離を合わせる",
-    Content: "助走３歩目に第２マークを置こう",
+    Content: "助走３歩目に第２マークを置く",
   },
   {
-    Title: "踏切角度と一歩前",
+    Title: "助走距離を合わせる2",
+    Content: "スタートは記憶して、助走６歩目と踏切４歩前にマークを置く",
+  },
+  {
+    Title: "踏切角度",
     Content:
-      "踏切角度をゆっくりした動作で覚え、支持足への乗り込みと振り上げ足の蹴り出しを覚える。低重心での歩行を行い一歩前転がしと蹴り出しを覚える。腕振りを使い全身で踏み切る。",
+      "踏切角度は３０度くらい。まずは窓などに映る自分を見て、次にゆっくり踏切動作をして覚える。このとき踏切足の膝は曲げずに足の裏全体で着くようにする。",
   },
   {
-    Title: "踏切ドリル",
-    Content: "踏切ドリル２歩、４歩、短助走による練習",
-  },
-  {
-    Title: "バウンディングとホッピング左右",
+    Title: "踏切手前から踏切まで",
     Content:
-      "グラグラせず力の入る場所を探す。けんけんからのホッピング、短助走、全助走",
+      "４歩前で最終的な助走距離を合わせる　３歩前で少し重心を下げる　２歩前でぐっと前に身体を押し出す　１歩前で引き上げる足をつま先で蹴り出す　踏切足を伸ばして足の裏全体をついて体重を乗せていき、１歩前で蹴り出した足を飛びたい方向に出す。身体を持ち上げるように腕を振り全身で踏み切る。",
   },
   {
-    Title: "腕振りドリル",
-    Content: "腕振りのドリル後、全助走による練習",
+    Title: "踏切のリズム",
+    Content: "- ♪ ♩ ♪ ♪ 　　ンタタンタタン　　　原、点、回、避！",
   },
   {
     Title: "試技間の過ごし方",
-    Content: "次の試技で身体が動くように上手に過ごそう。",
+    Content:
+      "身体が冷えないように気をつけよう。ジャンプや腿上げ、腕を回したりして身体が動くようにしておく。",
   },
   {
-    Title: "雨天時",
+    Title: "試技間の過ごし方（中学生）",
     Content:
-      "屋根の下でジャンプドリル、階段で腕振りだけで階段を登るジャンプ、バウンディング",
+      "跳躍→シューズを履き替える→ジャージを着る→踏切ドリル→疲れていればジョグで身体を温めるだけ→順番を確認する→体操や動的なストレッチ→スパイクを履く→ジャージを脱ぐ→流し→跳躍　雨の日は無理して屋根から出ず、冷えないように気をつけよう。",
+  },
+  {
+    Title: "雨天時の練習",
+    Content:
+      "屋根の下でジャンプドリル、階段で腕振りだけで階段を登るジャンプ、バウンディング、ホッピング",
+  },
+]
+const ModelLink = [
+  {
+    Title: "踏切ドリル１",
+    Href: "https://www.youtube.com/watch?v=iZm0nRtfPs8",
+  },
+  {
+    Title: "踏切ドリル（ギャロップ）",
+    Href: "https://www.youtube.com/watch?v=efJbqsio6J4",
+  },
+  {
+    Title: "踏切の姿勢",
+    Href: "https://www.youtube.com/watch?v=p1Lbskr-vbQ",
+  },
+  {
+    Title: "バウンディング苦手な人へ",
+    Href: "https://www.youtube.com/watch?v=sgph2UjPna8",
+  },
+
+  {
+    Title: "反り跳びの見本１",
+    Href: "https://youtu.be/l6NwuAsyQ88",
+  },
+  {
+    Title: "反り跳びの見本２",
+    Href: "https://m.youtube.com/watch?time_continue=7&v=S4ApLHFg8qc&feature=emb_logo",
+  },
+  {
+    Title: "はさみ跳びの見本",
+    Href: "https://youtu.be/HLkksNVSnuM",
+  },
+  {
+    Title: "かがみ跳びの見本",
+    Href: "https://www.jaaf.or.jp/files/upload/202003/jhs-003-007.pdf",
   },
 ]
 
@@ -60,6 +110,18 @@ const Plan: NextPage = () => {
             </Accordion>
           )
         })}
+        {ModelLink.map((item: any) => {
+          return (
+            <Anchor
+              key={item.Title}
+              href={item.Href}
+              className="mt-0 rounded-xl border p-6 pt-2 pb-2 text-left"
+            >
+              <Text className="text-xl font-bold">{item.Title}</Text>
+            </Anchor>
+          )
+        })}
+
         <FooterCom />
       </Layout>
     </>
